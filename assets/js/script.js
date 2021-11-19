8a0ca9f9bcf04e84a275fed2dc720aebbc590a79
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-19 14:48:11 -0600

    added tool-tips and fixed accordion button ids so that individual accordions open on single time-blocks only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+//enable bootstrap tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 //display current date at the top of the scheduler
 const displayCurrentDate = () => {
        $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
